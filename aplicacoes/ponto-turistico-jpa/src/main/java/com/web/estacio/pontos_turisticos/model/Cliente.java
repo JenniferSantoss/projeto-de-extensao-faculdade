8b6e3312f0e968279bfcd0961a9f20755306e92b
Jennifer <jennylima10@gmail.com>
2024-08-22 22:13:51 -0300

    criando classe cliente

---
+++ aplicacoes/ponto-turistico-jpa/src/main/java/com/web/estacio/pontos_turisticos/model/Cliente.java
@@ -0,0 +1,35 @@
+package com.web.estacio.pontos_turisticos.model;
+
+
+public class Cliente {
+
+    private Long cliente_id;
+
+    private String nome;
+    
+    private String email;
+
+   // data_nascimento DATE,
+
+    private String genero;
+
+    private String profissao;
+
+    private String pais_origem;
+
+    private String idioma;
+
+    //data_cadastro TIMESTAMP DEFAULT NOW(),
+
+    private String faixa_etaria;
+
+    private Double renda;
+
+    private String estado_civil;
+
+    private Integer frequencia_viagens;
+
+    private String tipo_viajant;
+
+    private String preferencia_acomodaca;
+}
